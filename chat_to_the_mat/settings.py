7409665074822b1d41c295b730435b8b7fd3a1ee
Daisy McGirr <mcgirr.daisy@gmail.com>
2021-01-24 13:17:19 +0000

    Created home app, view, template, urls and test

--- chat_to_the_mat/settings.py
+++ chat_to_the_mat/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -151,3 +152,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
