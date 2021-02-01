76ce27865e08466303381517260443c3c480da83
Daisy McGirr <mcgirr.daisy@gmail.com>
2021-02-01 13:18:57 +0000

    Added contact model and ran migrations

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Contact
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'contact_reason',
+        'name',
+        'email',
+        'comments',
+    )
+
+
+admin.site.register(Contact, CategoryAdmin)
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    """
+    A Contact model for staff to view users queries
+    """
+    contact_reason = models.CharField(max_length=100, null=False, blank=False)
+    name = models.CharField(max_length=80,
+                            null=False, blank=False)
+    email = models.CharField(max_length=80,
+                             null=True, blank=True)
+    comments = models.CharField(max_length=1500, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
