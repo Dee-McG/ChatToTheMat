a32bdc35d689557eab05d9d26a034084a168f516
Daisy McGirr <mcgirr.daisy@gmail.com>
2021-02-03 08:05:41 +0000

    Updated unit tests to reflect code changes

--- chat_rooms/tests.py
+++ chat_rooms/tests.py
@@ -2,8 +2,8 @@ from django.test import TestCase
 
 
 class TestViews(TestCase):
-    def test_chat_rooms_home_page(self):
+    def test_chat_rooms_redirect_when_not_logged_in(self):
         """ Test chat rooms home page renders correct page """
         response = self.client.get('/chat_rooms/')
         self.assertEqual(response.status_code, 200)
-        self. assertTemplateUsed(response, 'chat_rooms/chat_home.html')
+        self. assertTemplateUsed(response, 'home/index.html')
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,9 +1,23 @@
 from django.test import TestCase
+from .forms import EditProfileForm
 
 # Create your tests here.
 class TestViews(TestCase):
-    def test_home_page(self):
+    def test_profile_page_redirect_when_logged_out(self):
         """ Test profile page renders correct page """
         response = self.client.get('/profiles/')
         self.assertEqual(response.status_code, 200)
-        self. assertTemplateUsed(response, 'profiles/profile.html')
+        self. assertTemplateUsed(response, 'home/index.html')
+
+    def test_edit_profile_page_redirect_when_logged_out(self):
+        """ Test edit profile page renders correct page """
+        response = self.client.get('/profiles/edit_profile')
+        self.assertEqual(response.status_code, 301)
+
+
+
+class TestEditForm(TestCase):
+    """ Test to check that user field is required """
+    def test_edit_profile_form(self):
+        form = EditProfileForm({'name': 'Ella', 'location': 'Rivendale', 'bio': 'I am the Ice Queen'})
+        self.assertTrue(form.is_valid())
