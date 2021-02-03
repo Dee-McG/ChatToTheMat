5acb2ab0867bbebb8441d983bf4dccbe3cb4ed7d
Daisy McGirr <mcgirr.daisy@gmail.com>
2021-02-03 23:26:35 +0000

    Added logic to delete Chat entries when greater than 21. Added addition js to scroll down to bottom of the chat

--- chat_rooms/views.py
+++ chat_rooms/views.py
@@ -8,7 +8,8 @@ from .models import User
 from .forms import ChatForm
 
 def chat_home(request):
-    """ A view to return the chatrooms page """
+    """ A view to return the chatrooms page defaulting
+    to the general chat """
     if not request.user.is_authenticated:
         return render(request, 'home/index.html')
 
@@ -27,9 +28,16 @@ def chat_home(request):
 
         form = ChatForm(request.POST, instance=user)
         if form.is_valid(): 
+            # Prevent auto save and add in time field
             form = form.save(commit=False)
             form.time = datetime.now()           
             form.save()
+
+            # Count DB entries and delete oldest record
+            count = Chat.objects.count()
+            if count > 20:
+                Chat.objects.order_by('time')[0].delete()
+ 
             return redirect(reverse('chat_home'))
         else:
            messages.error(request, 'Error! Something went wrong. Your message was not sent.')
