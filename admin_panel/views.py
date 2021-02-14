4a9a1a846d9538189c05fe5fbaaf186cdfd7dee3
Daisy McGirr <mcgirr.daisy@gmail.com>
2021-02-14 16:54:34 +0000

    Added admin panel button to admin profiles

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 
 from contact.models import Contact
 
@@ -11,6 +12,8 @@ def admin_panel(request):
     """ View to return admin panel to superusers displaying
     Open contact queries """
     if not request.user.is_superuser:
+        messages.error(request,
+                       'Only admins can access the admin panel')
         return redirect(reverse('home'))
 
     contacts = Contact.objects.filter(
@@ -27,6 +30,8 @@ def admin_panel(request):
 def update_contact_status(request, contact_id):
     """ View to mark contact query as complete """
     if not request.user.is_superuser:
+        messages.error(request,
+                       'Only admins can update contact queries')
         return redirect(reverse('home'))
 
     contact = get_object_or_404(Contact, id=contact_id)
