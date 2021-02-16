d88f600b6977f708811234bc3217ef679cf94aed
Daisy McGirr <mcgirr.daisy@gmail.com>
2021-02-16 15:09:33 +0000

    Updated unittests and refactored user_profile view

--- chat_rooms/tests.py
+++ chat_rooms/tests.py
@@ -4,6 +4,5 @@ from django.test import TestCase
 class TestViews(TestCase):
     def test_chat_rooms_redirect_when_not_logged_in(self):
         """ Test chat rooms home page renders correct page """
-        response = self.client.get('/chat_rooms/')
-        self.assertEqual(response.status_code, 200)
-        self. assertTemplateUsed(response, 'home/index.html')
+        response = self.client.get('/chat_rooms/room/general')
+        self.assertEqual(response.status_code, 302)
--- profiles/tests.py
+++ profiles/tests.py
@@ -5,15 +5,9 @@ from .forms import EditProfileForm
 # Create your tests here.
 class TestViews(TestCase):
     def test_profile_page_redirect_when_logged_out(self):
-        """ Test profile page renders correct page """
-        response = self.client.get('/profiles/')
-        self.assertEqual(response.status_code, 200)
-        self. assertTemplateUsed(response, 'home/index.html')
-
-    def test_edit_profile_page_redirect_when_logged_out(self):
-        """ Test edit profile page renders correct page """
-        response = self.client.get('/profiles/edit_profile')
-        self.assertEqual(response.status_code, 301)
+        """ Test profile page redirects to home when logged out """
+        response = self.client.get('/profiles/user/')
+        self.assertEqual(response.status_code, 302)
 
 
 class TestEditForm(TestCase):
