716344d6f7544cc65363cd83466ff7be2bb9e9fa
Daisy McGirr <mcgirr.daisy@gmail.com>
2021-02-19 21:00:09 +0000

    Removed unused javascript and added refresh

--- chat_rooms/static/chat_rooms/js/chat.js
+++ chat_rooms/static/chat_rooms/js/chat.js
@@ -2,18 +2,4 @@ $(document).ready(function()
 {
     $("#chat_messages").scrollTop($("#chat_messages")[0].scrollHeight);
     
-    function refresh()
-    {
-        var div = $('#chat_messages'),
-            divHtml = div.html();
-
-        div.html(divHtml);
-
-        $("#chat_messages").scrollTop($("#chat_messages")[0].scrollHeight);
-    }
-
-    setInterval(function()
-    {
-        refresh()
-    }, 5000); // 5 secs
 })
